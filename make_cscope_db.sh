089d91ef70922e073a2220b9fa774ad42de85a35
hourenwang <hourenwang@china-liantong.com>
2017-06-07 13:44:12 +0800

    make cscope script support arguments

--- make_cscope_db.sh
+++ make_cscope_db.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 find $PWD -path .git -prune -o -name "*.h" -o -name "*.hpp" -o -name "*.c"-o -name "*.cc" -o -name "*.cpp" > cscope.files
-cscope -bkq -i cscope.files
+if [ -n "$*"  ];then
+    echo "extra args:\n  $@"
+fi
+cscope -bkq -i cscope.files $@
 # ctags -R
