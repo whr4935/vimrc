65c53b40560177625ee2269c51e2a101b78feb5a
harry wang <wanghourenrd@163.com>
2017-06-20 22:29:04 +0800

    make_cscope_db support for native project now

--- make_cscope_db.sh
+++ make_cscope_db.sh
@@ -1,6 +1,42 @@
 #!/bin/sh
 # add arguments for extra include path.
 
+native_project=0
+
+
+while test $# != 0
+do
+    case $1 in
+        -n|--native) native_project=1
+            shift
+            ;;
+
+        -h|--help)
+            echo -n "make_cscope_db.sh [args] [include path]\n"
+            echo "\t-n: is native project."
+            echo "\t-h: show this help."
+            shift
+            break
+            ;;
+
+        *) break
+            ;;
+    esac
+done
+
+# echo $native_project
+
+if [ $native_project -eq 1 ];then
+    cscope_arg=-bq
+    echo cscope_arg=$cscope_arg
+else
+    cscope_arg=-bkq
+fi
+
+
+# exit
+
+
 for i in $@;do
     arg="-I"$i" "
     args=$args$arg 
@@ -12,6 +48,11 @@ if [ -n "$args"  ];then
     echo "extra args:\n  $args"
 fi
 
-cscope -bkq -i cscope.files $args
+cscope $cscope_arg  -i cscope.files $args
 # ctags --c++-kinds=+px --fields=+iaS --extra=+q -L cscope.files
 ctags -R --c++-kinds=+px --fields=+iaS --extra=+q . $@
+
+
+
+
+
