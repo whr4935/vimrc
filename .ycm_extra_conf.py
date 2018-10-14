56f2852d3b38389eac3f87022e0c61550e1cab15
houren wang <wanghourenrd@163.com>
2018-10-14 19:28:44 +0800

    fix ycm config

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -41,15 +41,15 @@ flags = [
     '-isystem',
     '/usr/include/c++/7',
     '-isystem',
-    '/usr/include/x86_64-linux-gnu/c++/7'
+    '/usr/include/x86_64-linux-gnu/c++/7',
     '-isystem',
-    '/usr/include/c++/7/backward'
+    '/usr/include/c++/7/backward',
     '-isystem',
-    '/usr/lib/gcc/x86_64-linux-gnu/7/include'
+    '/usr/lib/gcc/x86_64-linux-gnu/7/include',
     '-isystem',
     '/usr/local/include',
     '-isystem',
-    '/usr/lib/gcc/x86_64-linux-gnu/7/include-fixed'
+    '/usr/lib/gcc/x86_64-linux-gnu/7/include-fixed',
     '-isystem',
     '/usr/include/x86_64-linux-gnu',
     '-isystem',
